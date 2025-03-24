a581a11bc610fc0eaa1af82a0540c691e6364592
skmayya1 <skandamayya4@gmail.com>
2025-03-24 23:07:34 +0530

    fix:auth

---
+++ app/api/health/route.ts
@@ -0,0 +1,5 @@
+export const GET = () => {
+    return new Response("ok", {
+        status: 200,
+    });
+};
--- lib/auth.ts
+++ lib/auth.ts
@@ -5,5 +5,19 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", 
-    })
+    }),
+    emailAndPassword: {  
+        enabled: true
+    },
+    socialProviders: { 
+       github: { 
+        clientId: process.env.GITHUB_CLIENT_ID as string, 
+        clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+       } ,
+       google:{
+        clientId: process.env.GOOGLE_CLIENT_ID as string, 
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+       },
+       
+    }, 
 });
